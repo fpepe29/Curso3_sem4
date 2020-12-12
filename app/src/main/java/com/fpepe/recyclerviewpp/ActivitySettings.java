66766947ea49db22a62cbc6cc1007930da1db6e4
Pep <nandopepe05@gmail.com>
2020-12-12 11:20:38 -0300

    Curso 3 - Semana 4

---
+++ app/src/main/java/com/fpepe/recyclerviewpp/ActivitySettings.java
@@ -0,0 +1,15 @@
+package com.fpepe.recyclerviewpp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.fpepe.recyclerviewpp.R;
+
+public class ActivitySettings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
---
+++ app/src/main/java/com/fpepe/recyclerviewpp/interfaces/IComunicaFragments.java
@@ -0,0 +1,14 @@
+package com.fpepe.recyclerviewpp.interfaces;
+
+public interface IComunicaFragments {
+
+    public void iniciarJuego();
+    public void llamarAjustes();
+    public void consultarRanking();
+    public void consultarInstrucciones();
+    public void gestionarUsuario();
+    public void consultarInformación();
+
+
+
+}
